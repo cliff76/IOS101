3a506bd8578e02fccc8d9539baaa0f4ca92be55e
Clifton Craig <clifton.craig@gmail.com>
2018-02-14 13:59:51 -0800

    Adds initial control.

---
+++ Exercise 1/Completed/TipCalculator/MyViewController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using CoreGraphics;
+using UIKit;
+
+namespace TipCalculator
+{
+    public class MyViewController : UIViewController
+    {
+        public MyViewController()
+        {
+        }
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            this.View.BackgroundColor = UIColor.Yellow;
+            UITextField totalAmount = new UITextField()
+            {
+                Frame = new CGRect(10, 20, View.Bounds.Width - 20, 35),
+                KeyboardType = UIKeyboardType.NumberPad,
+                BorderStyle = UITextBorderStyle.RoundedRect,
+                Placeholder = "Enter tip amount"
+            };
+            View.AddSubviews(totalAmount);
+        }
+
+    }
+}
